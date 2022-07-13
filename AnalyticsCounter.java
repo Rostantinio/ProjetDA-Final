b572f5c86faed2d605471d130c17663bbef460ed
Kamdem guy rostant <guyrostantkamdem@gmail.com>
2022-07-13 14:14:17 +0100

    Compte le nombre d'occurrence des symptoms

---
+++ AnalyticsCounter.java
@@ -0,0 +1,14 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+import java.util.*;
+
+
+public class AnalyticsCounter {
+
+	public static void main(String args[]) throws IOException {
+		//comptage du nombre d'occurrences de chaque symptome
+		ISymptomsCount counter = new SymptomsCounter();
+		Map<String, Integer> symptomsWithOccurrences = counter.countSymptoms(symptoms);
+	}
+}
