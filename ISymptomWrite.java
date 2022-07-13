7e6722316cd656716c2136ba693d5711333328d1
Kamdem guy rostant <guyrostantkamdem@gmail.com>
2022-07-13 14:06:46 +0100

    ecriture des symptoms dans le fichier

---
+++ ISymptomWrite.java
@@ -0,0 +1,8 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface ISymptomWrite {
+    void writeToFile(Map<String, Integer> symptomsWithOccurrences) throws IOException;
+}
