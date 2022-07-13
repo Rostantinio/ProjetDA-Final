eabd391338956b2a3eb880f544aa7f546cd07aa7
Kamdem guy rostant <guyrostantkamdem@gmail.com>
2022-07-13 13:51:50 +0100

    Lecture des symptoms

---
+++ AnalyticsCounter.java
@@ -0,0 +1,14 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+import java.util.*;
+
+
+public class AnalyticsCounter {
+
+	public static void main(String args[]) throws IOException {
+		//lecture des symptomes
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptoms = symptomReader.getSymptoms();
+	}
+}
